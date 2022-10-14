52025940cfc61a3665278d29af3fa7004d6a851d
Yussef raouf <yussef.raouf11@gmail.com>
2022-10-14 19:47:54 +0200

    1, 9

---
+++ 1. Two Sum.py
@@ -0,0 +1,11 @@
+
+def twoSum(nums, target):
+    values = dict()
+    for i, elem in enumerate(nums):
+        x = target - elem
+        if x in values:
+            return [values[x], i]
+        values[elem] = i
+    
+    return []
+
---
+++ 9. Palindrome Number.py
@@ -0,0 +1,9 @@
+def isPalindrome(x):
+        y = str(x)[::-1]
+        if x == y:
+            return 'true'
+        else:
+            return 'false'
+
+result = isPalindrome(-121)
+print(result)
