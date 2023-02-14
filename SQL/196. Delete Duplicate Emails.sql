4a19003e217b085d80ccb73db4a488c7a19eaffb
Yussef raouf <yussef.raouf11@gmail.com>
2023-02-14 12:59:43 +0200

    SQL

---
+++ SQL/196. Delete Duplicate Emails.sql
@@ -0,0 +1,5 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+
+DELETE a from Person a, Person B WHERE a.id > b.id and a.email=b.email;
+
