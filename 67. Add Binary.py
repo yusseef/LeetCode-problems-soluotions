d165ea309026c4146a53507cf21bc643ff6c9771
Yussef raouf <yussef.raouf11@gmail.com>
2023-02-14 12:27:57 +0200

    14/2

---
+++ 67. Add Binary.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        return bin(int(a, 2) + int(b, 2))[2:]
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
