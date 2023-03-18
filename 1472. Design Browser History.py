903fd41192e59fb4a922b8ed0e3c3a4ebfb7b9e0
Yussef raouf <yussef.raouf11@gmail.com>
2023-03-18 17:46:30 +0200

    18/3

---
+++ 1472. Design Browser History.py
@@ -0,0 +1,26 @@
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.history = [homepage]
+        self.current = 0
+
+    def visit(self, url: str) -> None:
+        self.history = self.history[:self.current+1] + [url]
+        self.current += 1
+
+    def back(self, steps: int) -> str:
+        self.current = max(0, self.current - steps)
+        return self.history[self.current]
+
+    def forward(self, steps: int) -> str:
+        self.current = min(len(self.history)-1, self.current + steps)
+        return self.history[self.current]
+
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
