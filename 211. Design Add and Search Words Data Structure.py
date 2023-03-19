8d607621be77fe23a746347a10b1fadd85431075
Yussef raouf <yussef.raouf11@gmail.com>
2023-03-19 07:48:19 +0200

    19/3

---
+++ 211. Design Add and Search Words Data Structure.py
@@ -0,0 +1,31 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+        
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()      
+
+    def addWord(self, word):
+        current_node = self.root
+        for character in word:
+            current_node = current_node.children.setdefault(character, TrieNode())
+        current_node.is_word = True
+        
+    def search(self, word):
+        def dfs(node, index):
+            if index == len(word):
+                return node.is_word
+               
+            if word[index] == ".":
+                for child in node.children.values():
+                    if dfs(child, index+1):
+                        return True
+                    
+            if word[index] in node.children:
+                return dfs(node.children[word[index]], index+1)
+            
+            return False
+    
+        return dfs(self.root, 0)
