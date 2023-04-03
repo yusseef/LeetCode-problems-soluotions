bc60de2306a609ac592c3df00d76b7170739a530
Yussef raouf <yussef.raouf11@gmail.com>
2023-04-03 12:38:09 +0200

    create 2 files and delete 1 file

---
+++ Leet-Code 75/205. Isomorphic Strings.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        dict = {}
+        
+        
+        for i in range(len(s)):
+            if s[i] in dict:
+                if dict[s[i]] != t[i]:
+                    return False
+            else:
+                if t[i] in dict.values():  
+                    return False
+                dict[s[i]] = t[i]
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Leet-Code 75/392. Is Subsequence.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return True if i == len(s) else False
+        
