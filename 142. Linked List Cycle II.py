eee0f581ea664bc2f1b94d48aae2493ab8a426ec
Yussef raouf <yussef.raouf11@gmail.com>
2023-04-05 10:23:28 +0200

    feat: create '142. Linked List Cycle II.py'

---
+++ 142. Linked List Cycle II.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+  def detectCycle(self, head: ListNode) -> ListNode:
+    slow = head
+    fast = head
+
+
+    while fast and fast.next:
+      slow = slow.next
+      fast = fast.next.next
+      if slow == fast:
+
+        slow = head
+        while slow != fast:
+          slow = slow.next
+          fast = fast.next
+        return slow
+
+
+    return None
